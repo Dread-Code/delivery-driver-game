83a7402f7513f087c454dff947d8ecfe9bd60783
DreadfulCrazy <lucasospina97@gmail.com>
2022-10-17 21:53:39 -0500

    Changing car´s sprite color with spriteRenderer

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1,0,0,1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
     [SerializeField] float destroyDelay = 0.5f;
+
     bool hasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -16,12 +26,14 @@ public class Delivery : MonoBehaviour
     {
         if (collision.tag == "Package" && !hasPackage)
         {
+            spriteRenderer.color = hasPackageColor;
             Destroy(collision.gameObject, destroyDelay);
             hasPackage = true;
         }
 
         if (collision.tag == "Customer" && hasPackage)
         {
+            spriteRenderer.color = noPackageColor;
             Debug.Log("Package delivered");
             hasPackage = false;
         }
