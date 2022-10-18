177497a4149377ac0353c2d9503868a056ba8b1b
DreadfulCrazy <lucasospina97@gmail.com>
2022-10-17 21:33:42 -0500

    Destroy method used for packages

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float destroyDelay = 0.5f;
     bool hasPackage;
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -13,9 +14,9 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Package")
+        if (collision.tag == "Package" && !hasPackage)
         {
-            Debug.Log("Package picke up");
+            Destroy(collision.gameObject, destroyDelay);
             hasPackage = true;
         }
 
